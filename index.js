bb8523b1aa87768d0b4bdf155fa66560b6b83ee1
Jasmin Duong <jasmind009@gmail.com>
2025-06-21 11:51:18 -0400

    added file limit

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ if (process.env.NODE_ENV !== "development") {
 }
 
 app.use(session(sessionOptions));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 UserRoutes(app);
 PostRoutes(app);
